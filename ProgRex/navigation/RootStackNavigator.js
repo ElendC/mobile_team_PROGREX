c21b5871e7f97325d480ffa0ad5ab66e44902d35
Elend Egid <elende@hotmail.no>
2023-10-15 22:20:55 +0200

    Example of navigation added in this TEST.file| Implement the same navigation to the main applicaion on our other git

---
+++ ProgRex/navigation/RootStackNavigator.js
@@ -0,0 +1,22 @@
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+import { Screen1 } from '../screens/page1'
+import {Index} from '../screens/index'
+
+
+const Root = createStackNavigator()
+
+const RootStackNavigator = () => {
+    return (
+      <NavigationContainer>
+        <Root.Navigator initialRouteName='Index'>
+          <Root.Screen name="ProgRex" component={Index} />
+          <Root.Screen name="Second Page" component={Screen1} />
+
+        </Root.Navigator>
+      </NavigationContainer>
+    )
+  }
+
+  export default RootStackNavigator;
---
+++ ProgRex/screens/page1.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export const Screen1 = ({ navigation, route }) => {
+  return <Text>This is the second page</Text>;
+};
