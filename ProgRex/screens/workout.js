19cb58d9d53b24d09b586877323b6ff73ef2d2a4
Elend Egid <elende@hotmail.no>
2023-10-18 20:59:14 +0200

    Some design examples pushed

---
+++ ProgRex/screens/workout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+export const Workout = ({ navigation, route }) => {
+  return(
+
+  <View style={styles.container}>
+
+    
+
+  </View>
+
+  ) 
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2D2D2D',
+    alignItems: 'center',
+    justifyContent: 'center',
+},
+
+})
